edd04a70202b8996db485608a527ff9eb52e15cf
Stephen Samuel Miller <mrstephenmiller@gmail.com>
2023-11-21 18:21:25 -0700

    Initial commit for grid display

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sudokuGrid = document.getElementById('sudoku-grid');
+
+    // Replace this with your Sudoku data loading logic
+    const sudokuData1 = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ];
+
+    // Replace this with your Sudoku data loading logic
+    const sudokuData2 = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ];
+
+
+    // Render the Sudoku grid
+    sudokuData.forEach((row, rowIndex) => {
+        row.forEach((value, colIndex) => {
+            const cell = document.createElement('div');
+            cell.classList.add('sudoku-cell');
+            cell.textContent = value !== 0 ? value : '';
+            sudokuGrid.appendChild(cell);
+
+            // Add event listener for future interaction
+            cell.addEventListener('click', () => {
+                // Handle cell click (you can add logic to update the value)
+                console.log(`Clicked on cell ${rowIndex + 1}, ${colIndex + 1}`);
+            });
+        });
+    });
+});
