bc1b530117e017b25da758da2ae5ad2b798c1e92
Stephen <StephenMiller@users.noreply.github.com>
2023-11-26 20:17:59 -0700

    Added several new classes to assist with organization of code. Added first step to logic for solving puzzle. (#8)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 let game = null
+let eventBus = null
 
 // Add a function to handle grid selection
 function selectGrid(gridIndex) {
-    game.startNewGame(gridIndex)
+    game.startNewGame(gridIndex);
+}
+
+function toggleHints() {
+    game.toggleHints();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    eventBus = new EventBus();
     game = new Game();
-});
+})
